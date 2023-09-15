a5fbcc08bb6d68a29f19e635f443b90062ab15a9
ghazala16 <ghazalajamal16@gmail.com>
2023-09-15 16:40:32 +0530

    odd even find

---
+++ bit_manipulation_odd_even.java
@@ -0,0 +1,11 @@
+public class bit_manipulation_odd_even {
+    public static void oddOreven(int n)
+            System.out.println("odd no");
+        }
+    }
+    public static void main(String[] args) {
+        oddOreven(3);
+        oddOreven(11);
+        oddOreven(14);
+    }
+}
