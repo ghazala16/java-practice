c67a9a83e6e05aa3134d3c558bb172a3f8072262
ghazala16 <ghazalajamal16@gmail.com>
2023-09-15 15:34:09 +0530

    Update permutation.java

--- permutation.java
+++ permutation.java
@@ -15,6 +15,16 @@ public class permutation
         char curr = str.charAt(i);
         //abcde=>"ab"+"de"="abde"
         String NewStr=str.substring(0, i)+str.substring(i+1);
+
+        //str.substring(0, i) extracts a substring from the beginning of 
+        //the str up to (but not including) the character at index i. 
+        //This effectively captures all characters in str before the 
+        //character at index i.
+
+//str.substring(i + 1) extracts a substring starting 
+//from the character immediately after the one at index i
+// until the end of the string str. This effectively captures all 
+//characters in str after the character at index i.
         findPermutation(NewStr,ans+curr);
     }
 }
@@ -23,4 +33,4 @@ public static void main(String[] args) {
     findPermutation(str,"");
 }
     }
-    
+
