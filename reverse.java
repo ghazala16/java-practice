8aa822a825f83d81c6830532c13b430b0d1e3b6f
ghazala16 <ghazalajamal16@gmail.com>
2023-10-02 23:52:36 +0530

    comments added

--- reverse.java
+++ reverse.java
@@ -5,7 +5,7 @@ public class reverse {
         // Reverse the array in-place
         for (int i = 0; i < arr.length / 2; i++) {
             int temp = arr[i];
-            arr[i] = arr[arr.length - i - 1];
+            arr[i] = arr[arr.length - i - 1];//code will move  right to left
             arr[arr.length - i - 1] = temp;
         }
 
