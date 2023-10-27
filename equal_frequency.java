f1975620425afbf7195e5d2a15f541956f0d7f79
ghazala16 <ghazalajamal16@gmail.com>
2023-10-27 15:55:57 +0530

    queue

---
+++ equal_frequency.java
@@ -0,0 +1,6 @@
+public class equal_frequency {
+    public static void main(String[] args) {
+        int nums[]={1,2,4};
+        int k=5;
+    }
+}
---
+++ st_max_area.java
@@ -0,0 +1,27 @@
+public class st_max_area {
+    //brute force
+    public static void main(String[] args) {
+        int width=1;
+        int height[]={2,1,5,6,9,3};
+        st_max_area(height,width);
+    }
+    public static int st_max_area(int height[],int width)
+    {
+
+       /*  int curr=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<height.length;i++)
+        {
+            int area=height[i] * width;
+            curr=area;
+        if(curr>max)
+        max=curr;
+        }
+        System.out.print(max);*/
+
+
+        if(height[]==0)
+        return -1;
+        
+}
+}
