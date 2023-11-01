edde83d089f779b4e20c49395c32ae9d485aa2f3
ghazala16 <ghazalajamal16@gmail.com>
2023-11-01 09:49:52 +0530

    OPTIMISED

--- checkSequence.java
+++ checkSequence.java
@@ -9,7 +9,22 @@ public class checkSequence {
     }
 
 
+//MORE OPTIMISED CODE--------------------------------
 
-
-    //The provided checkSequence function appears to be a simple implementation to check if a number x can be obtained by adding a sequence of arithmetic progression starting from a with a common difference d. The function iterates through the sequence using a loop and checks if any element in the sequence equals the given number x. If such an element is found, the function returns 1, indicating that the number can be obtained from the sequence. If no such element is found, the function returns 0.//1 2 7->o/p-----1
+    /*//The provided checkSequence function appears to be a simple implementation to check if a number x can be obtained by adding a sequence of arithmetic progression starting from a with a common difference d. The function iterates through the sequence using a loop and checks if any element in the sequence equals the given number x. If such an element is found, the function returns 1, indicating that the number can be obtained from the sequence. If no such element is found, the function returns 0.//1 2 7->o/p-----1
+    if (a == 0 && d == 0 && x == 0) {
+        return 1; // Sequence is [0, 0, 0]
+    }
+    
+    // Check if d is zero
+    if (d == 0) {
+        return (a == x) ? 1 : 0; // Check if a equals x
+    }
+    
+    // Check if x can be obtained from the arithmetic sequence
+    if ((x - a) % d == 0 && (x - a) / d >= 0) {
+        return 1;
+    } else {
+        return 0;
+    }*/
 }
